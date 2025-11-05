6bc6d4cdb95304fa92f844becfcf1d806bfe73e4
Rodrigo(Zara) <rodzmaciel21@gmail.com>
2025-11-05 10:18:30 -0300

    Preparação para deploy na Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração Vite para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/biya-studio/', // caminho do repositório no GitHub
+  base: '/', // Vercel serve o site na raiz do domínio
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-    open: true
+    outDir: 'dist'
   }
 })
